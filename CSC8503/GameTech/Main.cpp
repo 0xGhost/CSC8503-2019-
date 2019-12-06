6eb1bd3bc083a6d2297428314353617906497b26
BlackGun <417632129@qq.com>
2019-12-06 01:05:25 +0000

    Astart tut

--- CSC8503/GameTech/Main.cpp
+++ CSC8503/GameTech/Main.cpp
@@ -62,11 +62,28 @@ void TestNetworking() {
 vector<Vector3> testNodes;
 
 void TestPathfinding() {
+	NavigationGrid grid("TestGrid1.txt");
 
+	NavigationPath outPath;
+
+	Vector3 startPos(80, 0, 10);
+	Vector3 endPos(80, 0, 80);
+
+	bool found = grid.FindPath(startPos, endPos, outPath);
+
+	Vector3 pos;
+	while (outPath.PopWaypoint(pos)) {
+		testNodes.push_back(pos);
+	}
 }
 
 void DisplayPathfinding() {
+	for (int i = 1; i < testNodes.size(); ++i) {
+		Vector3 a = testNodes[i - 1];
+		Vector3 b = testNodes[i];
 
+		Debug::DrawLine(a, b, Vector4(0, 1, 0, 1));
+	}
 }
 
 
@@ -89,9 +106,9 @@ int main() {
 		return -1;
 	}
 
-	TestStateMachine();
-	//TestNetworking();
-	//TestPathfinding();
+	//TestStateMachine();
+	TestNetworking();
+	TestPathfinding();
 
 	w->ShowOSPointer(false);
 	w->LockMouseToWindow(true);
